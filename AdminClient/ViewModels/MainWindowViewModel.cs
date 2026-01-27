4d4633a9191ccd4afa37f324bfb0189eb57a16b6
deustenebrarum <gamesevolutionteam@gmail.com>
2026-01-27 21:17:45 +0300

    Add admin project

---
+++ AdminClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AdminClient.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ AdminClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AdminClient.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
